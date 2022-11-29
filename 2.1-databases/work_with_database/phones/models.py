d0bb07b2945c2dbe15195902feb17a4f57f01a1a
Vladimir Pogrebnyak <voldi112@gmail.com>
2022-11-30 00:54:05 +0100

    Выгрузка каталога товаров из csv-файла с сохранением всех позиций в базе данных

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,13 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    image = models.URLField(max_length=200)
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField()
+
+
